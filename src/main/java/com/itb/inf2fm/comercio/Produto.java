2948d938d8162f4d6555972243d4f4c9b95be9b7
Gustavo Gomes <cejutagu@gmail.com>
2023-06-06 12:29:48 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/Produto.java
@@ -0,0 +1,16 @@
+package com.itb.inf2fm.comercio;
+
+public class Produto {
+
+	
+        private Long id;
+        private String nome;
+        private String descricao; 
+        private String codigobarras;
+        private double preco;
+        
+        //public: Acesso liberado para todas as classes.
+        
+        //private 
+
+}
